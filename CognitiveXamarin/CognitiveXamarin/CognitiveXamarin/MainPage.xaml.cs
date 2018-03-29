1539550b72621b401576a96126358272b4c37936
Andrew Kenny <andrew.kenny@t4g.com>
2018-03-29 16:16:38 -0300

    Changed the form of the project. Added the cognitive service as a service. Not yet functioning, but shouldn't take to much to get there. Also added the general forms for the UI. Needs binding and needs to be hooked up the the service.

---
+++ CognitiveXamarin/CognitiveXamarin/CognitiveXamarin/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CognitiveXamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void Analysis_OnClicked(object sender, EventArgs e)
+	    {
+	        Navigation.PushAsync(new SummaryPage());
+	    }
+	}
+}
---
+++ CognitiveXamarin/CognitiveXamarin/CognitiveXamarin/Tools/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CognitiveXamarin.Tools
+{
+    static partial class Extensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+        {
+            foreach (var i in ie)
+            {
+                action(i);
+            }
+        }
+    }
+}
